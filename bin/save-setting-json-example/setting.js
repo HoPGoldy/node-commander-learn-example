6135c4826eaf9c2ff05cdccd54e1e53436bb2c2d
huang puguang <puguang_huang@fpi-inc.com>
2019-06-24 10:43:12 +0800

    fix: 导出模式

--- bin/save-setting-json-example/setting.js
+++ bin/save-setting-json-example/setting.js
@@ -8,20 +8,27 @@ const settingTempalte = {
     default: 'default value'
 }
 
-module.exports = {
-    init: () => {
-        try {
-            fs.statSync(settingFile)
-        }
-        catch (e) {
-            console.log('设置文件不存在, 已新建')
-            save(settingTempalte)
-        }
-    }, 
-    load: () => {
-        return JSON.parse(fs.readFileSync(settingFile, 'utf8'))
-    }, 
-    save: (data) => {
-        fs.writeFileSync(settingFile, JSON.stringify(data, null, 4))
+// 初始化项目
+function init() {
+    try {
+        fs.statSync(settingFile)
     }
+    catch (e) {
+        console.log('设置文件不存在, 已新建')
+        save(settingTempalte)
+    }
+}
+// 读取配置项
+function load() {
+    return JSON.parse(fs.readFileSync(settingFile, 'utf8'))
+}
+// 覆写配置项
+function save(data) {
+    fs.writeFileSync(settingFile, JSON.stringify(data, null, 4))
+}
+
+module.exports = {
+    init, 
+    load, 
+    save
 }
